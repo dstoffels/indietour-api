6168b2c3b3a3a1a98b2e380e578104f945afeae5
dstoffels <dan.stoffels@gmail.com>
2023-07-04 13:13:28 -0500

    fix: updated band model active band/tour

--- authentication/models.py
+++ authentication/models.py
@@ -6,14 +6,15 @@ from .utils import generate_verification_code
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    active_band = models.ForeignKey("bands.Band", on_delete=models.SET_NULL, null=True, default=None)
-    active_tour = models.ForeignKey("tours.Tour", on_delete=models.SET_NULL, null=True, default=None)
+    active_band = models.UUIDField(null=True, default=None)
+    active_tour = models.UUIDField(null=True, default=None)
     email = models.CharField(unique=True, max_length=255, null=False, editable=False)
     username = models.CharField(unique=False, max_length=255, null=False)
     email_verified = models.BooleanField(default=False)
     verification_code = models.CharField(max_length=6, default=generate_verification_code)
-    member_bands = models.ManyToManyField("bands.Band", through="bands.BandUser", related_name="users")
 
+    member_bands = models.ManyToManyField("bands.Band", through="bands.BandUser", related_name="users")
+    owned_bands = models.QuerySet
     contacts: models.QuerySet = None
 
     def get_bands(self):
